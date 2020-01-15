aefba63027a8a762d5e7a45815433520839c6b15
Witold Markowski <witold.a.markowski@gmail.com>
2020-01-15 21:38:44 +0000

    Select tile on touch. Drag the map.

--- klondiklon-core/src/main/java/com/github/wmarkow/klondiklon/Klondiklon.java
+++ klondiklon-core/src/main/java/com/github/wmarkow/klondiklon/Klondiklon.java
@@ -8,9 +8,18 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Plane;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.Ray;
 
+/***
+ * https://www.badlogicgames.com/wordpress/?p=2032
+ * https://github.com/libgdx/libgdx/blob/master/tests/gdx-tests/src/com/badlogic/gdx/tests/IsometricTileTest.java
+ * @author wmarkowski
+ *
+ */
 public class Klondiklon extends ApplicationAdapter implements InputProcessor
 {
     Texture texture;
@@ -18,6 +27,12 @@ public class Klondiklon extends ApplicationAdapter implements InputProcessor
     SpriteBatch batch;
     final Sprite[][] sprites = new Sprite[10][10];
     final Matrix4 matrix = new Matrix4();
+    final Plane xzPlane = new Plane(new Vector3(0, 1, 0), 0);
+    final Vector3 intersection = new Vector3();
+    Sprite lastSelectedTile = null;
+    final Vector3 curr = new Vector3();
+    final Vector3 last = new Vector3(-1, -1, -1);
+    final Vector3 delta = new Vector3();
 
     @Override
     public void create()
@@ -49,19 +64,21 @@ public class Klondiklon extends ApplicationAdapter implements InputProcessor
     public void render()
     {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        cam.update();           
-                        
+        cam.update();
+
         batch.setProjectionMatrix(cam.combined);
         batch.setTransformMatrix(matrix);
         batch.begin();
-        for(int z = 0; z < 10; z++) {
-                for(int x = 0; x < 10; x++) {
-                        sprites[x][z].draw(batch);
-                }
+        for (int z = 0; z < 10; z++)
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                sprites[x][z].draw(batch);
+            }
         }
         batch.end();
-        
-//        checkTileTouched();
+
+        checkTileTouched();
     }
 
     @Override
@@ -102,14 +119,24 @@ public class Klondiklon extends ApplicationAdapter implements InputProcessor
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button)
     {
-        // TODO Auto-generated method stub
+        last.set(-1, -1, -1);
         return false;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer)
     {
-        // TODO Auto-generated method stub
+        Ray pickRay = cam.getPickRay(screenX, screenY);
+        Intersector.intersectRayPlane(pickRay, xzPlane, curr);
+        
+        if(!(last.x == -1 && last.y == -1 && last.z == -1)) {
+                pickRay = cam.getPickRay(last.x, last.y);
+                Intersector.intersectRayPlane(pickRay, xzPlane, delta);                 
+                delta.sub(curr);
+                cam.position.add(delta.x, delta.y, delta.z);
+        }
+        last.set(screenX, screenY, 0);
+        
         return false;
     }
 
@@ -125,5 +152,24 @@ public class Klondiklon extends ApplicationAdapter implements InputProcessor
     {
         // TODO Auto-generated method stub
         return false;
+    }
+
+    private void checkTileTouched()
+    {
+        if (Gdx.input.justTouched())
+        {
+            Ray pickRay = cam.getPickRay(Gdx.input.getX(), Gdx.input.getY());
+            Intersector.intersectRayPlane(pickRay, xzPlane, intersection);
+            int x = (int) intersection.x;
+            int z = (int) intersection.z;
+            if (x >= 0 && x < 10 && z >= 0 && z < 10)
+            {
+                if (lastSelectedTile != null)
+                    lastSelectedTile.setColor(1, 1, 1, 1);
+                Sprite sprite = sprites[x][z];
+                sprite.setColor(1, 0, 0, 1);
+                lastSelectedTile = sprite;
+            }
+        }
     }
 }
