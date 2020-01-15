c9cc28240583ee2eaa422c9f3a7ffa6c0075109f
Witold Markowski <witold.a.markowski@gmail.com>
2020-01-15 19:46:10 +0000

    Display some time map 10x10

--- core/src/com/github/wmarkow/klondiklon/Klondiklon.java
+++ core/src/com/github/wmarkow/klondiklon/Klondiklon.java
@@ -2,38 +2,128 @@ package com.github.wmarkow.klondiklon;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector3;
 
-public class Klondiklon extends ApplicationAdapter
+public class Klondiklon extends ApplicationAdapter implements InputProcessor
 {
-    SpriteBatch batch;
-    Texture img;
+    Texture texture;
     OrthographicCamera cam;
+    SpriteBatch batch;
+    final Sprite[][] sprites = new Sprite[10][10];
+    final Matrix4 matrix = new Matrix4();
 
     @Override
     public void create()
     {
+        texture = new Texture("badlogic.jpg");
+        cam = new OrthographicCamera(10, 10 * (Gdx.graphics.getHeight() / (float) Gdx.graphics.getWidth()));
+        cam.position.set(5, 5, 10);
+        cam.direction.set(-1, -1, -1);
+        cam.near = 1;
+        cam.far = 100;
+        matrix.setToRotation(new Vector3(1, 0, 0), 90);
+
+        for (int z = 0; z < 10; z++)
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                sprites[x][z] = new Sprite(texture);
+                sprites[x][z].setPosition(x, z);
+                sprites[x][z].setSize(1, 1);
+            }
+        }
+
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+
+        Gdx.input.setInputProcessor(this);
     }
 
     @Override
     public void render()
     {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        cam.update();           
+                        
+        batch.setProjectionMatrix(cam.combined);
+        batch.setTransformMatrix(matrix);
         batch.begin();
-        batch.draw(img, 0, 0);
+        for(int z = 0; z < 10; z++) {
+                for(int x = 0; x < 10; x++) {
+                        sprites[x][z].draw(batch);
+                }
+        }
         batch.end();
+        
+//        checkTileTouched();
     }
 
     @Override
     public void dispose()
     {
         batch.dispose();
-        img.dispose();
+        texture.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode)
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode)
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character)
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button)
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button)
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer)
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY)
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount)
+    {
+        // TODO Auto-generated method stub
+        return false;
     }
 }
