790fa22a66a1e110502f633eedbae3e11ecdb615
Witold Markowski <witold.a.markowski@gmail.com>
2020-01-22 20:45:19 +0000

    Add some experimental code abut coordinates.

--- klondiklon-core/src/main/java/com/github/wmarkow/klondiklon/KKIsometricTiledMapRenderer.java
+++ klondiklon-core/src/main/java/com/github/wmarkow/klondiklon/KKIsometricTiledMapRenderer.java
@@ -28,14 +28,34 @@ import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.objects.TiledMapTileMapObject;
 import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 
 public class KKIsometricTiledMapRenderer extends IsometricTiledMapRenderer
 {
-
+    private Matrix4 isoTransform;
+    private Matrix4 invIsotransform;
+    
     public KKIsometricTiledMapRenderer(TiledMap map, float unitScale) {
         super(map, unitScale);
+        init();
     }
 
+    private void init () {
+        // create the isometric transform
+        isoTransform = new Matrix4();
+        isoTransform.idt();
+
+        // isoTransform.translate(0, 32, 0);
+        isoTransform.scale((float)(Math.sqrt(2.0) / 2.0), (float)(Math.sqrt(2.0) / 4.0), 1.0f);
+        isoTransform.rotate(0.0f, 0.0f, 1.0f, -45);
+
+        // ... and the inverse matrix
+        invIsotransform = new Matrix4(isoTransform);
+        invIsotransform.inv();
+}
+    
     @Override
     public void renderObjects(MapLayer layer)
     {
@@ -43,6 +63,20 @@ public class KKIsometricTiledMapRenderer extends IsometricTiledMapRenderer
         final float color = Color.toFloatBits(batchColor.r, batchColor.g, batchColor.b,
                 batchColor.a * layer.getOpacity());
 
+        final float layerOffsetX = layer.getRenderOffsetX() * unitScale;
+        // offset in tiled is y down, so we flip it
+        final float layerOffsetY = -layer.getRenderOffsetY() * unitScale;
+        
+//     // setting up the screen points
+//        // COL1
+//        topRight.set(viewBounds.x + viewBounds.width - layerOffsetX, viewBounds.y - layerOffsetY);
+//        // COL2
+//        bottomLeft.set(viewBounds.x - layerOffsetX, viewBounds.y + viewBounds.height - layerOffsetY);
+//        // ROW1
+//        topLeft.set(viewBounds.x - layerOffsetX, viewBounds.y - layerOffsetY);
+//        // ROW2
+//        bottomRight.set(viewBounds.x + viewBounds.width - layerOffsetX, viewBounds.y + viewBounds.height - layerOffsetY);
+        
         for (MapObject object : layer.getObjects())
         {
             if (object == null)
@@ -59,11 +93,15 @@ public class KKIsometricTiledMapRenderer extends IsometricTiledMapRenderer
 
             TextureRegion region = tiledMapTileMapObject.getTextureRegion();
 
+            Vector3 res = translateScreenToIso(new Vector2(tiledMapTileMapObject.getX(), tiledMapTileMapObject.getY()));
+            
+//            float x1 = res.x * unitScale;
+//            float y1 = res.y * unitScale;
             float x1 = tiledMapTileMapObject.getX() * unitScale;
             float y1 = tiledMapTileMapObject.getY() * unitScale;
             float x2 = x1 + region.getRegionWidth() * unitScale;
             float y2 = y1 + region.getRegionHeight() * unitScale;
-
+            
             float u1 = region.getU();
             float v1 = region.getV2();
             float u2 = region.getU2();
@@ -96,4 +134,13 @@ public class KKIsometricTiledMapRenderer extends IsometricTiledMapRenderer
             batch.draw(region.getTexture(), vertices, 0, NUM_VERTICES);
         }
     }
+    
+    private Vector3 translateScreenToIso (Vector2 vec) {
+        Vector3 screenPos = new Vector3();
+        
+        screenPos.set(vec.x, vec.y, 0);
+        screenPos.mul(invIsotransform);
+
+        return screenPos;
+}
 }
