8c5146d0fce7e2e5c88d0d84cbc98d4707674756
Witold Markowski <witold.a.markowski@gmail.com>
2020-01-24 19:27:11 +0000

    Rework home.tmx a bit.

--- klondiklon-core/src/main/resources/home.tsx
+++ klondiklon-core/src/main/resources/home.tsx
@@ -1,5 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="grass" tilewidth="80" tileheight="70" tilecount="3" columns="3">
- <grid orientation="isometric" width="80" height="70"/>
- <image source="home.png" width="240" height="70"/>
+<tileset version="1.2" tiledversion="1.3.1" name="home" tilewidth="160" tileheight="95" tilecount="6" columns="3">
+ <image source="home.png" width="480" height="190"/>
 </tileset>
