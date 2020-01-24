b6bbf06ec7ff4a4fa63c798c433cf0b1548ab34c
Witold Markowski <witold.a.markowski@gmail.com>
2020-01-24 22:38:21 +0000

    Add fir tree.

---
+++ klondiklon-core/src/main/resources/object_fir.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="object_fir" tilewidth="158" tileheight="383" tilecount="1" columns="1">
+ <image source="object_fir.png" width="158" height="383"/>
+</tileset>
