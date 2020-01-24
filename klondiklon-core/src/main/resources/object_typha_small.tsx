cdcd550caef768e366da67fa060330301b22a85c
Witold Markowski <witold.a.markowski@gmail.com>
2020-01-24 19:38:32 +0000

    Add a typha object.

---
+++ klondiklon-core/src/main/resources/object_typha_small.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="object_typha_small" tilewidth="53" tileheight="84" tilecount="1" columns="1">
+ <image source="object_typha_small.png" width="53" height="84"/>
+</tileset>
