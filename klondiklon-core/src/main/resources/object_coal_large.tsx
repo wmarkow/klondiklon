db1288b5c5fe1db927bba09204eceea19b8e7d93
Witold Markowski <witold.a.markowski@gmail.com>
2020-01-25 21:27:34 +0000

    Add coal objects.

---
+++ klondiklon-core/src/main/resources/object_coal_large.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="object_coal_large" tilewidth="194" tileheight="377" tilecount="1" columns="1">
+ <image source="object_coal_large.png" width="194" height="377"/>
+</tileset>
---
+++ klondiklon-core/src/main/resources/object_coal_small.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="object_coal_small" tilewidth="120" tileheight="87" tilecount="1" columns="1">
+ <image source="object_coal_small.png" width="120" height="87"/>
+</tileset>
