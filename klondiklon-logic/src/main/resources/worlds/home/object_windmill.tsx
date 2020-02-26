4774907e29d0603ad217367ce1a0b4cae3ea1c24
Witold Markowski <witold.a.markowski@gmail.com>
2020-02-26 19:30:51 +0000

    Improve windmill animation.

--- klondiklon-logic/src/main/resources/worlds/home/object_windmill.tsx
+++ klondiklon-logic/src/main/resources/worlds/home/object_windmill.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="object_windmill" tilewidth="510" tileheight="501" tilecount="3" columns="3">
- <image source="windmill.png" width="1530" height="501"/>
+<tileset version="1.2" tiledversion="1.3.1" name="object_windmill" tilewidth="479" tileheight="516" tilecount="3" columns="3">
+ <image source="windmill.png" width="1437" height="516"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="150"/>
