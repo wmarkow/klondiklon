cf39d85a698a1a1ebe77c09bf3d8e485a62991fd
Witold Markowski <witold.a.markowski@gmail.com>
2020-02-26 19:10:51 +0000

    Add possibility to override a default directory for Gdx local files.

--- klondiklon-desktop/src/main/java/com/github/wmarkow/klondiklon/desktop/DesktopLauncher.java
+++ klondiklon-desktop/src/main/java/com/github/wmarkow/klondiklon/desktop/DesktopLauncher.java
@@ -1,20 +1,25 @@
 package com.github.wmarkow.klondiklon.desktop;
 
+import java.io.File;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglFiles;
 import com.github.wmarkow.klondiklon.HomeLand;
 
 public class DesktopLauncher
 {
     public static void main(String[] arg)
     {
+        LwjglFiles.localPath = System.getProperty("user.home") + File.separator + ".klondiklon" + File.separator;
+
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.resizable = true;
         config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width;
         config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
-//        config.width = 1024;
-//        config.height = 768;
-        
+        // config.width = 1024;
+        // config.height = 768;
+
         new LwjglApplication(new HomeLand(), config);
     }
 }
