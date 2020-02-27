f47062c13cca980fee51fc60d1d6a4ee51dd4b09
Witold Markowski <witold.a.markowski@gmail.com>
2020-02-27 17:41:41 +0000

    Add integratio test for reading TMX and writing it back.

---
+++ klondiklon-logic/src/integration-test/java/com/github/wmarkow/klondiklon/MapReadWriteTest.java
@@ -0,0 +1,39 @@
+package com.github.wmarkow.klondiklon;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.mapeditor.core.Map;
+import org.mapeditor.io.TMXMapReader;
+import org.mapeditor.io.TMXMapWriter;
+
+public class MapReadWriteTest
+{
+
+    @Test
+    public void testReadWrite() throws IOException
+    {
+        File file = new File("target/classes/worlds/home/home.tmx");
+        TMXMapReader tmxMapReader = new TMXMapReader();
+        Map tmxMap = null;
+        try
+        {
+            tmxMap = tmxMapReader.readMap(file.getAbsolutePath());
+        } catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        TMXMapWriter tmxMapWriter = new TMXMapWriter();
+        File finalDst = new File("target/tests/worlds/home/home2.tmx");
+        finalDst.getParentFile().mkdirs();
+        finalDst.delete();
+        
+        File tempDst = new File("target/classes/worlds/home/home2.tmx");
+
+        tmxMapWriter.writeMap(tmxMap, tempDst.getAbsolutePath());
+        FileUtils.moveFile(tempDst, finalDst);
+    }
+}
