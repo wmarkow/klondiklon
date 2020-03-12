ca698c810cc33b9698bfac3085fbf2cf19fca21a
Witold Markowski <witold.a.markowski@gmail.com>
2020-03-12 23:08:11 +0100

    Fix garden cell object.

--- klondiklon-logic/src/main/resources/worlds/home/object_garden_cell.tsx
+++ klondiklon-logic/src/main/resources/worlds/home/object_garden_cell.tsx
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.1" name="garden_cell" tilewidth="152" tileheight="99" tilecount="1" columns="1">
- <image source="garden_cell.png" width="152" height="99"/>
+ <image source="object_garden_cell.png" width="152" height="99"/>
  <tile id="0">
   <properties>
-   <property name="SPRITE_TYPE" value="GARDEN"/>
+   <property name="TYPE" value="GARDEN"/>
   </properties>
  </tile>
 </tileset>
