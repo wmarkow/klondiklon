fb4417a811d67731604bbac15abd206f255fcfb9
Witold Markowski <witold.a.markowski@gmail.com>
2020-03-13 16:53:38 +0100

    Draw the wheat plant in the garden.

--- klondiklon-logic/src/main/java/com/github/wmarkow/klondiklon/map/objects/GardenCellObject.java
+++ klondiklon-logic/src/main/java/com/github/wmarkow/klondiklon/map/objects/GardenCellObject.java
@@ -2,19 +2,22 @@ package com.github.wmarkow.klondiklon.map.objects;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.github.wmarkow.klondiklon.ServiceRegistry;
 import com.github.wmarkow.klondiklon.graphics.TexturesRegistrar;
 
 public class GardenCellObject extends KKMapObject
 {
-    private Texture wheatGardenCell1;
+    private TextureRegion wheatGardenCell1;
 
     public GardenCellObject(TiledMapTile tile, int id, String objectType) {
         super(tile, id, objectType);
 
-        wheatGardenCell1 = ServiceRegistry.getInstance().getTexturesManager()
+        Texture textureWheat1 = ServiceRegistry.getInstance().getTexturesManager()
                 .getTexture(TexturesRegistrar.OBJECT_WHEAT_GARDEN_1);
+
+        wheatGardenCell1 = new TextureRegion(textureWheat1);
     }
 
     @Override
@@ -22,7 +25,8 @@ public class GardenCellObject extends KKMapObject
     {
         // draw a default garden cell
         super.drawTextures(batch, layerOpacity, unitScale);
+
         // draw the plant
-        // batch.draw(wheatGardenCell1, spriteVertices, offset, count);
+        drawTexture(batch, wheatGardenCell1, layerOpacity, unitScale);
     }
 }
