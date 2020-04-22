45cefe3a6f3dc5a13d49b121432e99f8c99f8f32
Witold Markowski <witold.a.markowski@gmail.com>
2020-04-22 16:05:52 +0100

    Introduce a preliminary version of android client. It is able to run 'gradlew build', however it doesn't compile.

---
+++ klondiklon-android/android/src/com/github/wmarkow/klondiklon/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.github.wmarkow.klondiklon;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.wmarkow.klondiklon.Klondiklon;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Klondiklon(), config);
+	}
+}
