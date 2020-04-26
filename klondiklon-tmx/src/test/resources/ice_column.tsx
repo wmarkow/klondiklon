11c4823da2a7c67f98bd2f2a1ca3970270bfdd7c
Witold Markowski <witold.a.markowski@gmail.com>
2020-04-26 17:02:52 +0100

    Add missing TSX files.

---
+++ klondiklon-tmx/src/test/resources/ice_column.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="ice_column" tilewidth="221" tileheight="454" tilecount="1" columns="1">
+ <image source="object_ice_column.png" width="221" height="454"/>
+ <tile id="0">
+  <properties>
+   <property name="TYPE" value="ICE_COLUMN"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ klondiklon-tmx/src/test/resources/object_rock_large.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="object_rock_large" tilewidth="178" tileheight="190" tilecount="1" columns="1">
+ <image source="object_rock_large.png" width="178" height="190"/>
+ <tile id="0">
+  <properties>
+   <property name="TYPE" value="ROCK_LARGE"/>
+  </properties>
+ </tile>
+</tileset>
